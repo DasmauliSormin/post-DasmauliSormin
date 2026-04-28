842661c5b58a06ed7628a4fb3ae8ed71d8978685
DasmauliSormin <dasmauli06@gmail.com>
2026-04-28 14:31:09 +0700

    12S24007

--- src/model/Model2.java
+++ src/model/Model2.java
@@ -1,5 +1,20 @@
 package model;
+//dasmauli sormin
 
 public class Model2 {
-	
+    private String kategori;
+    private int stok;
+
+    public Model2(String kategori, int stok) {
+        this.kategori = kategori.toUpperCase();
+        this.stok = stok;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public int getStok() {
+        return stok;
+    }
 }
